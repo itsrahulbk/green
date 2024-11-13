4e7781cad492d002d2b683fc3110091dc75ca4c8
Rahul Babu K <rahulbabuk05@gmail.com>
2024-11-13 08:27:10 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/green',
+        destination: '/green',
+      },
+    ]
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
