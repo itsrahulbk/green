e57465ffe5a54687d5c69d0cca090b3b609fa23b
Rahul Babu K <rahulbabuk05@gmail.com>
2024-11-13 10:44:24 +0530

    Add /green route and configuration for rahulbk.com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
-
+  return (
+    <div className="min-h-screen bg-black" role="main" aria-label="Black background">
+      {/* This page intentionally left blank except for the black background */}
+    </div>
+  )
 }
